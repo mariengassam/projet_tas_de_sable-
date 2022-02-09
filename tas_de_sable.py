b58ceffed9618d272645a2e2deff47767340aec3
Marie NGASSAM <marie.ngassam2@gmail.com>
2022-02-09 15:54:58 +0100

    commit

---
+++ tas_de_sable.py
@@ -0,0 +1,9 @@
+# projet_tas_de_sable-
+#projet sur la simulation de l’écoulement d’un tas de sable
+#########################################
+# groupe MIASHS
+# Marie NGASSAM
+# Sami BOUHAFSI
+# Zakariya EL HADJAJI 
+# https://github.com/mariengassam/projet_tas_de_sable-#readme
+#########################################
